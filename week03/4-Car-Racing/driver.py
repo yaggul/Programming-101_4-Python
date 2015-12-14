ec6dc2aaf200a3e8d5bb6e2ca480deb2dc7314c1
yaggul <krommul@gmail.com>
2015-12-14 16:34:39 +0200

    Create driver.py

---
+++ week03/4-Car-Racing/driver.py
@@ -0,0 +1,7 @@
+class Driver:
+
+    def __init__(self, name, obj):
+        self._name = name
+        self._car = obj._car
+        self._model = obj._model
+        self._max_speed = obj._max_speed
